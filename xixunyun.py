2396c12b387173bfdc590ef35e69b94f93cffc84
kuilei0926 <1120391530@qq.com>
2020-12-15 10:50:32 +0800

    Update xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -28,7 +28,7 @@ comment = "" # 签到说明（如需换行请使用\\n，如需输入"\"斜杠�
 no_wait = 0 # 是否不等待直接完成（将取消获取真实位置信息功能，习训云会报告“位置区域”），0：等待，1：不等待
 
 system = os.environ["SYSTEM"] 
-if system == 11:
+if system == "":
     system = "10" # 模拟Android版本号
 else:
     system = os.environ["SYSTEM"] # 模拟Android版本号
