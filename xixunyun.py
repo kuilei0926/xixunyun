982a02ce987f4fc7c26d462a8a77608a2421a002
kuilei0926 <1120391530@qq.com>
2020-12-18 15:04:42 +0800

    Update xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -5,6 +5,48 @@ import json
 import time
 import sys
 import os
+import base64
+import rsa
+
+class HandleSign:
+    # 定义服务器公钥, 往往可以存放在公钥文件中
+    server_pub = """
+        -----BEGIN PUBLIC KEY-----
+        MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlYsiV3DsG+t8OFMLyhdmG2P2J4GJwmwb1rKKcDZmTxEphPiYTeFIg4IFEiqDCATAPHs8UHypphZTK6LlzANyTzl9LjQS6BYVQk81LhQ29dxyrXgwkRw9RdWaMPtcXRD4h6ovx6FQjwQlBM5vaHaJOHhEorHOSyd/deTvcS+hRSQIDAQAB
+        -----END PUBLIC KEY-----
+        """
+
+    @classmethod
+    def to_encrypt(cls, msg, pub_key=None):
+        """
+        非对称加密
+        :param msg: 待加密字符串或者字节
+        :param pub_key: 公钥
+        :return: base64密文字符串
+        """
+        if isinstance(msg, str):            # 如果msg为字符串, 则转化为字节类型
+            msg = msg.encode('utf-8')
+        elif isinstance(msg, bytes):        # 如果msg为字节类型, 则无需处理
+            pass
+        else:                               # 否则抛出异常
+            raise TypeError('msg必须为字符串或者字节类型!')
+
+        if not pub_key:                     # 如果pub_key为空, 则使用全局公钥
+            pub_key = cls.server_pub.encode("utf-8")
+        elif isinstance(pub_key, str):      # 如果pub_key为字符串, 则转化为字节类型
+            pub_key = pub_key.encode('utf-8')
+        elif isinstance(pub_key, bytes):    # 如果msg为字节类型, 则无需处理
+            pass
+        else:                               # 否则抛出异常
+            raise TypeError('pub_key必须为None、字符串或者字节类型!')
+
+        public_key_obj = rsa.PublicKey.load_pkcs1_openssl_pem(pub_key)  # 创建 PublicKey 对象
+
+        cryto_msg = rsa.encrypt(msg, public_key_obj)  # 生成加密文本
+        cipher_base64 = base64.b64encode(cryto_msg)   # 将加密文本转化为 base64 编码
+
+        return cipher_base64.decode()   # 将字节类型的 base64 编码转化为字符串类型
+    
 # 配置开始
 user = os.environ["USER"]
 account = user.split( )[0] # 账号1
@@ -42,7 +84,7 @@ if len(model) == 0:
 # 配置结束
 longitude = sign_gps.split(",")[0] # 经度
 latitude = sign_gps.split(",")[1] # 纬度
-print (sign_gps + "\n" + longitude + "\n" + latitude)
+print (longitude + "\n" + latitude)
 def isset(v): 
     try : 
         type (eval(v)) 
@@ -84,7 +126,6 @@ if no_wait==0:
     msg += '请确认一下哦~\n'
 else:
     regeopage = {"status":"1","info":"OK","infocode":"10000","regeocode":{"formatted_address":[],"addressComponent":{"country":[],"province":[],"city":[],"citycode":[],"district":[],"adcode":[],"township":[],"towncode":[],"streetNumber":{"street":[],"number":[],"location":"0,0","direction":[],"distance":[]}}}}
-    print("你将会用账号",account,"在经度：",longitude,"，纬度"+latitude+"以",remark_name,"进行签到。(｀・ω・´)")
     msg += '你将会用账号' + account + '在经度：' + longitude + '，纬度' + latitude + '以' + remark_name + '进行签到。(｀・ω・´)\n'
 msg += '我们来登录吧！\n'
 headers = {
@@ -150,6 +191,10 @@ if accountpage["code"]==20000: # 成功
             print(msg)
             exit()
         print (regeopage["regeocode"]["addressComponent"]["province"])
+        cryto_info = HandleSign.to_encrypt(longitude)
+        cryto_info = HandleSign.to_encrypt(latitude)
+        print(longitude)
+        print(latitude)
         signactdata_origin = {"change_sign_resource":"0","longitude":longitude,"latitude":latitude,"comment":"","remark":remark,"address":regeopage["regeocode"]["formatted_address"],"address_name":(regeopage["regeocode"]["addressComponent"]["province"]+regeopage["regeocode"]["addressComponent"]["city"]+regeopage["regeocode"]["addressComponent"]["district"])}
         print("开始组合数据包：",signactdata_origin)
         if no_wait==0:
