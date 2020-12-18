70e3fabc1638227d1c14bbf34931def0c57fc5c3
kuilei0926 <1120391530@qq.com>
2020-12-18 14:27:56 +0800

    Update xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -7,7 +7,7 @@ import sys
 import os
 # 配置开始
 user = os.environ["USER"]
-account = user.split( )[0] # 账号1
+account = user.split( )[0] # 账号
 password = user.split( )[1] # 密码
 school_id = user.split( )[2] # 学校ID
 print (account,password,school_id)
