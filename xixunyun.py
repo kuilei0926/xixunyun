0f0b827e8ae6fdf4b94dec7409b09cd6f1254de3
kuilei0926 <1120391530@qq.com>
2020-12-21 17:57:24 +0800

    Update xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -1,11 +1,23 @@
 import requests
 import json
+
+# 配置开始
+user = os.environ["USER"]
+account = user.split( )[0] # 账号1
+password = user.split( )[1] # 密码
+school_id = user.split( )[2] # 学校ID
+if account=="" or account=="" or school_id=="" or sign_gps=="":
+    print("诶呀？好像你还没有配置好账号信息和签到设置呢！(>_<)\n")
+    exit(1)
+sign_gps = os.environ["SIGN_GPS"]  # 签到坐标（注意小数点取后6位）
+longitude = sign_gps.split(",")[0] # 经度
+latitude = sign_gps.split(",")[1] # 纬度
 data = {
-  "account":"31702160115",
-  "password":"160115",
-  "school_id":"924",
-  "longitude":"114.243681",
-  "latitude":"22.696406",
+  "account":account,
+  "password":password,
+  "school_id":school_id,
+  "longitude":"longitude",
+  "latitude":"latitude",
   "address_name":"深圳市朗生建材有限公司"
 }
 headers = {'Content-Type': 'application/json'}
