427fbb4d3ca9a096ee0320b52acaae52885e95d1
kuilei0926 <1120391530@qq.com>
2020-12-21 17:54:14 +0800

    Update xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -12,4 +12,3 @@ headers = {'Content-Type': 'application/json'}
 
 response = requests.post(url='http://xxy.kuileii.cn/release/xixunyun', headers=headers, data=json.dumps(data))
 print(response.json())
-print(response.text)
